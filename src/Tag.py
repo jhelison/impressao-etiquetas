19b6f91f5a9de9de0d1c216627b0f3ccf455a575
Aiack <jhelisong@gmail.com>
2020-09-29 13:36:45 -0300

    Code refacturation

--- src/Tag.py
+++ src/Tag.py
@@ -4,30 +4,29 @@ import math
 def cmToPixel(cms):
     return int(cms * 370.7952755906)
 
-ETIWIDHT = cmToPixel(6.35)
-ETIHEIGHT = cmToPixel(3.1)
 
-PAPERWIDHT = cmToPixel(21)
-PAPERHEIGHT = cmToPixel(29.7)
-
-class etiqueta():
-    def __init__(self):
-        self.paper = None
+class Tag():    
+    def __init__(self, productName, value, code):
+        self.TAGWIDHT = cmToPixel(6.35)
+        self.TAGHEIGHT = cmToPixel(3.1)
         
-        self.buildPaper()
+        self.img = None
         
+        self.productName = productName
+        self.value = value
+        self.code = code
         
-    def buildEtiqueta(self):
-        img = Image.new('RGBA', (ETIWIDHT, ETIHEIGHT), 'yellow')
+    def buildImg(self):
+        self.img = Image.new('RGBA', (self.TAGWIDHT, self.TAGHEIGHT), 'yellow')
+        self.idraw = ImageDraw.Draw(self.img)
         
-        idraw = ImageDraw.Draw(img)
-        
-        #O Tijolão
+    def addLogoText(self):
         font = ImageFont.truetype("arialbi.ttf", size=150)
-        center_text = int(math.floor((ETIWIDHT / 2) - (font.getsize("O TIJOLÂO")[0] / 2)))
-        idraw.text((center_text, 60), "O TIJOLÂO", 'black', font)
+        center_text = int(math.floor((self.TAGWIDHT / 2) - (font.getsize("O TIJOLÂO")[0] / 2)))
+        self.idraw.text((center_text, 60), "O TIJOLÂO", 'black', font)
         
-        #Nome do Produto
+    def addProductText(self):
+        pass
         product = "CHAV. TIPO BIELA FURO PASS. 12X12MM 40X135 UNIFORT"
         font = ImageFont.truetype("arial.ttf", size=175)
         
@@ -35,9 +34,8 @@ class etiqueta():
         
         lines = []
         if numLines == 3:
+            substringIndex = 0
             for index, char in enumerate(list(product[:23])):
-            lines.append(product[:23])
-            lines.append(product[23:46])
         
                 
         print(len(product))
@@ -59,15 +57,6 @@ class etiqueta():
                 center_text = int(math.floor((ETIWIDHT / 2) - (font.getsize(text)[0] / 2)))
                 textHeight = 60 + 150 + index * 200
                 idraw.text((center_text, textHeight), text, 'black', font)
+    
         
-        return img
-        
-    def buildPaper(self):
-        self.paper = Image.new('RGBA', (PAPERWIDHT, PAPERHEIGHT), 'white')
-        
-        
-        self.paper.paste(self.buildEtiqueta(), (int((PAPERWIDHT / 2) - ETIWIDHT / 2), int((PAPERHEIGHT / 2) - ETIHEIGHT / 2)))
-        
-        self.paper.save('img.png')
-        
-        
+    
